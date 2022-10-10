485e5b8c15a4f84bae964168295f07f06527ced7
Danny Neel <dneel10@gmail.com>
2022-10-10 14:02:05 -0400

    add ability for human codemaker to create a custom code

--- game.rb
+++ game.rb
@@ -1,13 +1,14 @@
 class Game
   attr_accessor :name, :player1, :player2, :code, :compare_array, :color_options, :game_won
-
+  @@code = []
   def initialize
     @name = "Mastermind"
     @player1 = Codemaker.new
     @player2 = Codebreaker.new
     @game_won = false
+
+    # hash will increment/decrement as colors are selected/guessed in the game.
     @color_options = {"blue" => 0, "red" => 0, "green" => 0, "yellow" => 0, "orange" => 0, "purple" => 0}
-    @code = []
 
     @compare_array = []
 
@@ -16,9 +17,12 @@ class Game
 
   private
 
-  def computer?(player)
-    if player.name == "Computer"
-      player.pick_random_code(@color_options, @code)
+  def computer?(player1, player2)
+    if player1.name == "Computer" && player1.role == "Codemaker"
+      player1.pick_random_code(@color_options, @@code)
+      puts "#{code}"
+    elsif player2.name == "Computer" && player2.role == "Codebreaker"
+      player1.make_code
       puts "#{code}"
     end
   end
@@ -36,7 +40,7 @@ class Game
   end
 
   def play_game
-    computer?(@player1)
+    computer?(@player1, @player2)
     loop do 
       play_round
       if game_won
@@ -56,7 +60,11 @@ class Game
     puts "Would you like to play again? [Y/N]"
     replay = gets.chomp
     if replay == "Y"
-      @code = []
+      player1.code = []
+      # reset color options Hash to 0
+      @color_options.each do |key, value|
+        @color_options[key] = 0
+      end
       play_game
     else
       puts "Thanks for playing!"
@@ -69,10 +77,11 @@ class Game
     @compare_array = []
   end
 
+  # may need to break black/white into two separate functions to ensure not overlap
   def check_code(code, guess, hash)
     # check if each position in guess matches the code
     guess.each_with_index do |v, i|
-      if guess[i] == code[i]
+      if guess[i] == player1.code[i]
         compare_array.push("Black")
         # decrement the value in the colors hash by 1
         hash[guess[i]] -= 1
@@ -106,10 +115,12 @@ class Player
 end
 
 class Codemaker < Player
+  attr_accessor :role, :code
 
   def initialize
     @name = get_codemaker_name
     @role = "Codemaker"
+    @code = []
   end
   
   def pick_random_code(hash, array)
@@ -118,9 +129,7 @@ class Codemaker < Player
       # increment the value for each color in the hash by 1 if it was selected.
       hash[array[i]] += 1
       puts "#{hash[array[i]]}"
-    end
-
-  
+    end  
   end
 
   def get_codemaker_name
@@ -128,14 +137,18 @@ class Codemaker < Player
     gets.chomp
   end
 
+  # allow codemaker to make a custom code
   def make_code
-    # allow codemaker to make a custom code
+    4.times do |i|
+      puts "Place the position #{i} color: " 
+      code.push(gets.chomp)
+    end
   end
 
 end
 
 class Codebreaker < Player
-  attr_accessor :guess
+  attr_accessor :guess, :role
 
   def initialize
     @name = get_codebreaker_name
@@ -157,3 +170,4 @@ class Codebreaker < Player
 end
 
 Game.new
+mm
